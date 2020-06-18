8c24e0afec907b29d93f9f72fd4de73a8c709208
Kandam13 <h1rokmd13@gmail.com>
2020-06-18 16:52:03 +0900

    006のsqlを作成

---
+++ db/db-challenge3/db-006/db-006.sql
@@ -0,0 +1,16 @@
+SELECT
+    id
+,   email
+,   password
+,   name
+,   introduction
+,   cell_phone_number
+,   work_phone_number
+,   is_deleted
+,   created_at
+,   modified_at
+FROM
+    users
+ORDER BY
+    id ASC
+;
